f907620479cbb123a904341d21946adb6ade24ec
shubhamgosavi155 <shubhamgosavi155@gmail.com>
2023-06-25 23:12:58 +0530

    first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    throw new Error('Method not implemented.');
+  }
   title = 'dark-mode';
+  color: any;
+  letters: any;
+  document: any;
+
+  ondarkMode() {
+    const element = document.body;
+    element.classList.toggle('dark-mode');
+  }
 }
